d193c00c0cb091a6f807cbc7c70197159b05ad10
Manzoor <159796587+manzoor30@users.noreply.github.com>
2024-06-11 00:38:14 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+// Ben
+public class Main {
+    public static void main(String[] args) { 
+        // Create an instance of YourClassName
+        theApp app = new theApp();
+        app.theApp();
+    }
+}
